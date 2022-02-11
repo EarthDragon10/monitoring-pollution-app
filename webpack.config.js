0f5d351f063729861dac5eb71fb119c6e002d84f
Laguda Stefano <lagudadev@gmail.com>
2022-02-11 11:59:42 +0100

    feat: added webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presents: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
